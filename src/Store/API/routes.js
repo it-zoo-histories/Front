be70c07a53621216f2c6dc0cc714df08458ad0b5
kubitre <kubitre@gmail.com>
2019-05-19 14:03:35 +0700

    fix route api to coordinates

--- src/Store/API/routes.js
+++ src/Store/API/routes.js
@@ -1,4 +1,4 @@
-const APIRoute = "http://it-zoo.pro:8888";
+const APIRoute = "https://it-zoo.pro:8888";
 export const APIAuthentification = APIRoute + "/tururu";
 export const APIRoutesGetLast = APIRoute + "/routes"
 export const APIRoutesGetRouteByID = APIRoute + "/routes?id=";

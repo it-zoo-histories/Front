a4daf563baf05cda93591104510bda8ea9d757e1
kubitre <kubitre@gmail.com>
2019-05-19 13:47:18 +0700

    ref change api routes

--- src/Store/API/constants.js
+++ src/Store/API/constants.js
@@ -1,10 +1,10 @@
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://it-zoo.pro:8080';
 
-export const API_BASE_ROUTER = "http://localhost:8888";
+export const API_BASE_ROUTER = "https://it-zoo.pro:8443";
 
 export const ACCESS_TOKEN = "access_token";
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost/oauth2/redirect';
+export const OAUTH2_REDIRECT_URI = 'https://it-zoo.pro/oauth2/redirect';
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
